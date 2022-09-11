317a83beae12e02dce44e45ff782e414d330e533
Ayringov <Ayringovender15@gmail.com>
2022-09-12 01:15:09 +0200

    added hero class

---
+++ GADE6122part1/GADE6122part1/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122part1
+{
+    class Hero : Character
+    {
+        public Hero(int x, int y, int hp) : base(x, y, TileType.Hero)
+        {
+            this.damage = 2;
+        }
+
+
+        public override Movement ReturnMove(Movement move = 0)
+        {
+            int dir = (int)move - 1;
+            Tile tile = vision[dir];
+
+            if (tile != null)
+            {
+                if (tile.Type == TileType.Empty)
+                {
+                    return move;
+                }
+            }
+
+            return Movement.No_Movement;
+
+
+        }
+
+        public override string ToString()
+        {
+            return "player stats: \n" +
+                "hp: " + hp + "/" + maxHp + "\n" +
+                "Damage: " + damage + "\n" +
+                "[" + x + "," + y + "]";
+        }
+
+    }
+}
