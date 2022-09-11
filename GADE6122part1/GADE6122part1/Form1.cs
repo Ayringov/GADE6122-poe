be92ced7e728dab8bb64a733eedc4dcf27432d0b
Ayringov <Ayringovender15@gmail.com>
2022-09-11 19:55:52 +0200

    test

--- GADE6122part1/GADE6122part1/Form1.cs
+++ GADE6122part1/GADE6122part1/Form1.cs
@@ -16,5 +16,10 @@ namespace GADE6122part1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            test;
+        }
     }
 }
